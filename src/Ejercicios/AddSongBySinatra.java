c7137b4a292956197bd7aac79eea04b47b53bbac
Nagochi1 <nagochi@hotmail.com>
2018-05-23 20:24:41 -0500

    Continuación de ejercicio

--- src/Ejercicios/AddSongBySinatra.java
+++ src/Ejercicios/AddSongBySinatra.java
@@ -4,26 +4,42 @@ import org.junit.Test;
 
 public class AddSongBySinatra extends ParentTestSinatra {
 	
-	//@Test
-	//public void autenticacionPagina() {
-	//	logIn(usuario,pwd);
-	//}
+	@Test
+	public void autenticacionPagina() {
+		logIn(usuario,pwd);
+	}
 	
-	//@Test
-	//public void addSong() {
-	//	logIn(usuario,pwd);
-	//	agregarCancion(titulo,duracion,fecha,letra);
-	//	existeCancion(titulo,duracion,fecha,letra);
-	//}
+	@Test
+	public void addSong() {
+		logIn(usuario,pwd);
+		agregarCancion(titulo,duracion,fecha,letra);
+		existeCancion(titulo,duracion,fecha,letra);
+	}
+	
+	@Test
+	public void likeSong() {
+		buscarCancion(titulo);
+		darLike();
+	}
 		
+	@Test
+	public void editSong() {
+		logIn(usuario,pwd);
+		buscarCancion(titulo);
+		editarCancion(tituloEditado);
+		
+	}
+	
+	
 	@Test
 	public void deleteSong() {
 		logIn(usuario,pwd);
-		//buscarCancion(titulo);
+		buscarCancion(titulo);
 		eliminarCancion(titulo);
-			
-			
-		}
+				
+	}
+
+	
 
 
 		
